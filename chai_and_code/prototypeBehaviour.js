10e9fa29d3fba62740e5d1c586413f7d6f0d2436
Anushrey Jain <anushrey1998@gmail.com>
2023-09-02 14:56:52 +0530

    new kayword

---
+++ chai_and_code/prototypeBehaviour.js
@@ -0,0 +1,10 @@
+// JS has prototype behaviour.
+// i.e it kees on searching laye by layer means on parents, grandParent and so onn
+// until it gets null value also known as Prototypal Inheritance.
+
+// Protoype givs us the following - 
+// this keyword
+// classes
+// we don't have this keyword inside arrow function
+// prototypal inheritance
+
